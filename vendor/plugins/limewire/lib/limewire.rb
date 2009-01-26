aed586ee3b3d9fc10b136875b8ae71764846490b
Michael Nutt <michael@nuttnet.net>
2009-01-26 16:46:55 -0500

    Add mojito to limewire-ruby; make rails plugins reload themselves on every request

--- vendor/plugins/limewire/lib/limewire.rb
+++ vendor/plugins/limewire/lib/limewire.rb
@@ -372,6 +372,47 @@ module Limewire
     end
   end
 
+  # Mojito is LimeWire's distributed hash table implementation.  It is a large table of
+  # key-value pairs that is shared among the nodes on the network.
+  class Mojito
+    # The name of the hash table
+    def self.name
+      Core::MojitoManager.name
+    end
+
+    # Are we able to connect to the DHT?
+    def self.running?
+      Core::MojitoManager.running?
+    end
+
+    # Send a command to the DHT. To see the output of the command, provide a block.
+    #
+    # Example:
+    #
+    #   >> LimeWire::Mojito.handle("help") do |output|
+    #   *>   puts output
+    #   *> end
+    #
+    def self.handle(command, &block)
+      writer = RubyWriter.new(&block)
+      Core::MojitoManager.handle(command, writer)
+    end
+
+    class RubyWriter < java.io.PrintWriter
+      def initialize(&block)
+        @block = block
+      end
+
+      def print(line)
+        @block.call line
+      end
+
+      def println(line)
+        print(line)
+      end
+    end
+  end
+
   # The Filterable module does brute-force filtering on an array via +find_all+.  It mixes itself into the
   # result in order to enable chaining filters.
   module Filterable

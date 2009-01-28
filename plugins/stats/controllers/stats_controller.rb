3a884d60c6109df92d62a3efc49ef4a2002d585c
Derek Haas <derekhaas@gmail.com>
2009-01-28 16:12:28 -0500

    /stats/tests can return (html|txt)

--- plugins/stats/controllers/stats_controller.rb
+++ plugins/stats/controllers/stats_controller.rb
@@ -6,7 +6,12 @@ class StatsController < PluginController
   
   def tests
     if RAILS_ENV == 'development'
-      render :text => UnitTests.run.to_html
+      case params[:id]
+      when 'txt'
+        render :text => UnitTests.run.to_s
+      else
+        render :text => UnitTests.run.to_html
+      end
     else
       render :text => "Run rails in the development environment to access unit tests"
     end

f56efae7d5f3b7913a1deaa21e634a18284647e1
Michael Nutt <michael@nuttnet.net>
2009-08-03 13:40:27 -0400

    Streaming previews in songbird plugin

--- plugins/songbird_search/public/javascripts/search.js
+++ plugins/songbird_search/public/javascripts/search.js
@@ -3,10 +3,14 @@ jQuery(document).ready(function(){
 
   var query = $.getURLParam('q');
   $('window').attr("title", "Search for \"" + query + "\"");
-  //submitSearch(query);
+  // submitSearch(query);
   startComet(query);
   $('#results').dblclick(triggerDownload);
+  $('#results').click(triggerPreview);
+
 });
+
+var urnCache = [];
 
 var startComet = function(guid) {
   $.cometd.publish('/search', guid);
@@ -33,7 +37,7 @@ var receiveSearchResult = function(result) {
     var treeItem = $(document.createElement('treeitem')).addClass('result').attr('id', result.sha1);
     var treeRow = $(document.createElement('treerow'));
 
-    treeRow.append($(document.createElement('treecell')).addClass('space').attr('label', ' '));
+    treeRow.append($(document.createElement('treecell')).addClass('space').attr('label', '▸').attr('properties', 'defaultHide'));
 
     if(result.title && result.author) {
       treeRow.append($(document.createElement('treecell')).addClass('name').attr('label', result.author + " - " + result.title));
@@ -71,4 +75,42 @@ var triggerDownload = function(click) {
   var sha1 = $(row).attr('id');
   var guid = $('#results treechildren').attr('id');
   $.post('/downloads', {urn: sha1, guid: guid}, function(guid) {});
+};
+
+var triggerPreview = function(click) {
+  var sel = click.currentTarget.currentIndex;
+  var row = $('#results treeitem').get(sel);
+  var left = $('#space-label').get(0).clientLeft;
+  var right = left + $('#space-label').get(0).clientWidth;
+
+  if(click.clientX > left && click.clientX < right) {
+    var urn = row.id;
+    urnCache.push(urn);
+    var guid = $('#results treechildren').attr('id');
+    var url = "/stream/stream.mp3?guid="+guid+"&urn="+urn;
+    console.log(url);
+    $(row).find('treecell:first').attr("properties", "playing");
+    soundManager.stopAll();
+    var previewSound = soundManager.createSound({
+      id: urn,
+      url: url,
+      volume: 100,
+      whileplaying: function() {
+	var time = getTime(this.position, true);
+	var label = '▸' + " " + time;
+	var el = $('#'+this.sID+' treecell:first');
+	el.attr('label', label);
+      }
+    });
+    previewSound.play();
+  }
+};
+
+var getTime = function(nMSec,bAsString) {
+    // convert milliseconds to mm:ss, return as object literal or string
+    var nSec = Math.floor(nMSec/1000);
+    var min = Math.floor(nSec/60);
+    var sec = nSec-(min*60);
+    // if (min == 0 && sec == 0) return null; // return 0:00 as null
+    return (bAsString?(min+':'+(sec<10?'0'+sec:sec)):{'min':min,'sec':sec});
 };

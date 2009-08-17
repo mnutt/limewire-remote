c7ff385dd4c8b7222a78d29e434936cbde1797b0
Michael Nutt <michael@nuttnet.net>
2009-08-17 12:18:48 -0400

    Make search creation more resilient

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,13 +2,19 @@ class SearchController < ApplicationController
   self.allow_forgery_protection = false
 
   def create
-    search = Limewire::Search.query(params[:query])
-    search.start
-    render :json => {:guid => search.guid.to_s}
+    if(params[:query])
+      search = Limewire::Search.query(params[:query])
+      search.start
+      render :json => {:guid => search.guid.to_s}
+    else
+      render :json => {:error => "No search term specified"}
+    end
   end
 
   def index
-    render :json => JSON.pretty_generate({ :searches => Limewire::Search.all })
+    render :json => JSON.pretty_generate({ :searches => Limewire::Search.all.map{|s| { :query_string => s.query_string,
+		                                                                                   :result_count => s.search_results.size.to_s,
+		                                                                                   :guid => s.query_guid.to_s } } })
   end
 
   def show

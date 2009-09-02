63c094ebc2f3ad2284f3f47b57a1f404723009bf
Michael Nutt <michael@nuttnet.net>
2009-09-02 16:33:12 -0400

    Fix broken file importing on startup

--- app/models/file_desc.rb
+++ app/models/file_desc.rb
@@ -8,15 +8,16 @@ class FileDesc < ActiveRecord::Base
   end
 
   def self.create_from_file(file)
-    file = Limewire::File.new(file)
-    record = self.find_by_sha1urn(file.getSHA1Urn.to_s) || self.new
+    lwfile = Limewire::File.new(file)
+    return if file.respond_to?(:getSHA1Urn) && self.find_by_sha1urn(file.getSHA1Urn)
+    record = self.new
     record.sha1urn = file.getSHA1Urn.to_s
     record.is_rare = file.rare_file?
     record.file_name = file.file_name
-    record.title = file.metadata.title
-    record.album = file.metadata.album
-    record.genre = file.metadata.genre
-    record.artist = file.metadata.artist
+    record.title = lwfile.metadata.title
+    record.album = lwfile.metadata.album
+    record.genre = lwfile.metadata.genre
+    record.artist = lwfile.metadata.artist
     record.size = file.file_size
     record.path = file.path
     record.is_store = file.store_file?
@@ -25,16 +26,16 @@ class FileDesc < ActiveRecord::Base
     record.completed_upload_count = file.completed_uploads
     record.attempted_upload_count = file.attempted_uploads
     record.hit_count = file.hit_count
-    record.license = file.metadata.license.to_s
-    record.year = file.metadata.year
-    record.comment = file.metadata.comment
-    record.track = file.metadata.track
-    record.bitrate = file.metadata.bitrate
-    record.length = file.metadata.length
-    record.sample_rate = file.metadata.sample_rate
-    record.width = file.metadata.width
-    record.height = file.metadata.height
-    record.language = file.metadata.language
+    record.license = lwfile.metadata.license.to_s
+    record.year = lwfile.metadata.year
+    record.comment = lwfile.metadata.comment
+    record.track = lwfile.metadata.track
+    record.bitrate = lwfile.metadata.bitrate
+    record.length = lwfile.metadata.length
+    record.sample_rate = lwfile.metadata.sample_rate
+    record.width = lwfile.metadata.width
+    record.height = lwfile.metadata.height
+    record.language = lwfile.metadata.language
     record.save!
     record
   end
